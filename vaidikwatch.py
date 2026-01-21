f5aa93bb639a0484d1e5c675d8132a75109859c3
Gaurav Singh Yadav <137716439+EGAVSIV@users.noreply.github.com>
2026-01-21 22:02:52 +0530

    Update vaidikwatch.py

--- vaidikwatch.py
+++ vaidikwatch.py
@@ -484,7 +484,7 @@ with right:
             f"{pos[p]:.2f}°",
             SIGNS[int(pos[p]//30)],
             f"{nak} (पद {pada})",
-            "↺🔴 वक्री" if retro[p] else 🟢↻"
+            "↺🔴 वक्री" if retro[p] else 🟢"
         ])
 
     nak, lord, pada = nakshatra_pada(pos["केतु"])
@@ -493,7 +493,7 @@ with right:
         f"{pos['केतु']:.2f}°",
         SIGNS[int(pos["केतु"]//30)],
         f"{nak} (पद {pada})",
-        "↺🔴 वक्री" if retro["केतु"] else 🟢↻"
+        "↺🔴 वक्री" if retro["केतु"] else 🟢"
     ])
 
     st.table(pd.DataFrame(
